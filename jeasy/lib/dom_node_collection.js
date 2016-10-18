4406d72083e87619c2bef2e10d0eea8d0811be27
Eugene Cheng <ugene1213@gmail.com>
2016-10-18 14:10:56 -0400

    initial commit

---
+++ jeasy/lib/dom_node_collection.js
@@ -0,0 +1,134 @@
+class DOMNodeCollection {
+  constructor(array) {
+    this.array = array;
+  }
+
+  html() {
+    if (arguments.length === 1) {
+      this.array.forEach( (el) => {
+        el.innerHTML = arguments[0];
+      });
+    } else {
+      return this.array[0].innerHTML;
+    }
+  }
+
+  empty() {
+    this.html("");
+  }
+
+  append(arg) {
+    if (arg instanceof DomNodeCollection) {
+      for (var i = 0; i < this.array.length; i++) {
+        for (var j = 0; j < arg.array.length; j++) {
+          this.array[i].innerHTML += arg.array[j].outerHTML;
+        }
+      }
+    } else if (typeof arg === 'string') {
+      for (let j = 0; j < this.array.length; j++) {
+        this.array[j].innerHTML += arg;
+      }
+    } else if (arg instanceof HTMLElement) {
+      for (let j = 0; j < this.array.length; j++) {
+        this.array[j].innerHTML += arg.outerHTML;
+      }
+    }
+  }
+
+
+  attr(attributeName) {
+    return this.array[0].getAttribute(attributeName);
+  }
+
+  addClass(newClass) {
+    this.array.forEach( (el) => el.classList.add(newClass));
+  }
+
+  removeClass(oldClass) {
+    this.array.forEach( (el) => el.classList.remove(oldClass));
+  }
+
+  children() {
+    let allTheChildren = [];
+    this.array.forEach( (el) => {
+      const queue = [el];
+
+      while (queue.length > 0) {
+
+        const node = queue.shift();
+        const children = Array.from(node.children);
+
+        while (children.length > 0) {
+          let child = children.pop();
+          allTheChildren.push(child);
+          queue.push(child);
+        }
+      }
+    });
+    return new DomNodeCollection(allTheChildren);
+  }
+
+  parent(){
+    const allTheParents = [];
+    for (var i = 0; i < this.array.length; i++) {
+      let currentEl = this.array[i];
+      while (currentEl.parentNode !== null) {
+        allTheParents.push(currentEl.parentNode);
+        currentEl = currentEl.parentNode;
+      }
+    }
+
+    return allTheParents;
+  }
+
+  find(selector) {
+    const matchingNodes = [];
+    this.each ( node => {
+      const matching = node.querySelectorAll(selector);
+      matchingNodes = matchingNodes.concat(Array.from(matching));
+    });
+
+    return new DOMNodeCollection(matchingNodes);
+  }
+
+  on(type, listener) {
+
+    this.forEach( node => {
+      const handlerKey = `jEasy key - ${type}`;
+      if (typeof node[handlerKey] === 'undefined') {
+        node[handlerKey] = [];
+      }
+
+      node[handlerKey].push(listener);
+    });
+  }
+
+  // come back to this later
+  off(type, listener) {
+
+    this.forEach(node => {
+      const handlerKey = `jEasy key - ${type}`;
+      node[handlerKey] = [];
+    });
+  }
+
+}
+
+
+
+
+
+
+// this.array.forEach( (el) => {
+//   if (el.children.length === 0) {
+//     return [];
+//   } else {
+//     const el.children + ;
+//
+//   }
+ // }
+// ul.append(collection)
+
+
+
+module.exports = DOMNodeCollection;
---
+++ jeasy/lib/main.js
@@ -0,0 +1,35 @@
+const DOMNodeCollection = require('./dom_node_collection.js') ;
+
+let documentReady = false;
+const documentReadyCallbacks = [];
+
+const $l = function(arg) {
+
+  let arrayify = [];
+
+  if (typeof arg === "string") {
+    const allSelectors = document.querySelectorAll(arg);
+    arrayify = arrayify.concat(Array.from(allSelectors));
+  }
+  //How the hell would you even be able to test this?
+  else if (arg instanceof HTMLElement) {
+    arrayify.push(arg);
+  }
+
+  else if (typeof arg === 'function') {
+    saveDocReadyCallback(arg);
+  }
+
+  return new DomNodeCollection(arrayify);
+};
+
+
+const saveDocReadyCallback = function(callback) {
+  if (documentReady === false) {
+    documentReadyCallbacks.push();
+  } else {
+    callback();
+  }
+};
+
+window.$l = $l;
